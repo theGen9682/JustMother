24882727e7b9f97d16590ddeccd941311fd9fe85
Digen Sharmma <sharmadigen@gmail.com>
2019-07-16 02:35:34 +0530

    Scan Bookings are now visible in database with some incomplete work, which will be done shortly. For some reason only Lab 1 is selected, working to resolve that.

--- app/src/main/java/com/example/pregnancy/ScanLabsBooking.kt
+++ app/src/main/java/com/example/pregnancy/ScanLabsBooking.kt
@@ -1,104 +1,101 @@
 package com.example.pregnancy
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.RadioButton
-import android.widget.TextView
+import android.widget.*
+import com.example.pregnancy.dataModels.ScanBookings
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_scan_labs_booking.*
+import android.widget.Toast
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class ScanLabsBooking : AppCompatActivity() {
 
-    private val db = FirebaseFirestore.getInstance()
+    private lateinit var database:DatabaseReference
 
-
-    var selectedLab: String = "Lab 1"
-
-
-    fun onRadioButtonClicked(view: View) {
-        if (view is RadioButton) {
-            // Is the button now checked?
-            val checked = view.isChecked
-
-            // Check which radio button was clicked
-            when (view.getId()) {
-                R.id.lab1Button ->
-                    if (checked) {
-                        // Pirates are the best
-
-                        selectedLab = "Lab 1"
-
-
-                    }
-                R.id.lab2Button ->
-                    if (checked) {
-
-                        selectedLab = "Lab 2"
-
-
-                    }
-            }
-        }
-    }
-
-
+    @SuppressLint("SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan_labs_booking)
 
+
         val scanName = intent.getStringExtra("ScanName")
 
 
-        val timeSlot = findViewById<TextView>(R.id.time)
-        val date = findViewById<TextView>(R.id.date)
+        val timeSlot = findViewById<EditText>(R.id.time)
+        val dateSlot = findViewById<EditText>(R.id.date)
 
-        val scanTime = timeSlot.text.toString()
-        val scanDate = date.text.toString()
+        val bookNow=findViewById<Button>(R.id.BookNowBtn)
 
 
-        Log.d("Selected Lab\n\n","\n\n------------------------------------------------------------------------------------------$selectedLab")
+        val labs= arrayOf("Lab 1", "Lab 2")
 
-        addData(scanDate,scanTime,scanName)
+        val spinner=findViewById<Spinner>(R.id.spinner)
+
+
+        if(spinner!=null)
+        {
+
+            val arrayAdapter=ArrayAdapter(this,R.layout.dropdown_list_textview,labs)
+            spinner.adapter=arrayAdapter
+
+            spinner.onItemSelectedListener=object:AdapterView.OnItemSelectedListener{
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    Toast.makeText(this@ScanLabsBooking,"Please Select a Lab",Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onItemSelected(parent: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+                    Toast.makeText(this@ScanLabsBooking,"Lab Selected",Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        }
+
+        val selectedLab=spinner.selectedItem.toString()
+
+        Log.d("SelectedLab","--------------------------------------------------------$selectedLab")
+
+
+        database=FirebaseDatabase.getInstance().reference
+
+        bookNow.setOnClickListener {
+
+            val scanTime = timeSlot.text.toString()
+            val scanDate = dateSlot.text.toString()
+
+            Log.d("Scan Time\n\n","------------------------------------------------------------\n$scanTime")
+            Log.d("Scan Time\n\n","------------------------------------------------------------\n$scanTime")
+
+
+            writeInDatabase(selectedLab, "Digen", scanName, scanTime, scanDate)
+            Toast.makeText(this@ScanLabsBooking,"Test Booked",Toast.LENGTH_SHORT).show()
+
+        }
+
+
+
+
+
+
 
 
 
     }
 
+    private fun writeInDatabase(lab:String,name:String, scanName:String?, timeSlot:String, date:String ) {
 
-    private fun addData(scanDate:String, scanTime:String, scanName:String)
-    {
-        db.collection("Scan Bookings")
-            .document(selectedLab)
-            .collection("Scan Dates")
-            .add(scanDate)
+        val scanDetails=ScanBookings(scanName,timeSlot,date)
 
-        db.collection("Scan Bookings")
-            .document(selectedLab)
-            .collection("Scan Dates")
-            .document(scanDate.toString())
-            .collection("Time Slot")
-            .document(scanTime.toString())
-            .collection("Booking Details")
-            .add("Digen Sharma")
+        database.child("Scan Bookings").child(lab).child("Bookings").child(name).setValue(scanDetails)
 
-
-        db.collection("Scan Bookings")
-            .document(selectedLab)
-            .collection("Scan Dates")
-            .document(scanDate.toString())
-            .collection("Time Slot")
-            .document(scanTime.toString())
-            .collection("Booking Details")
-            .document("Digen Sharma")
-            .set(scanName)
-            .addOnSuccessListener {
-                Log.d("Successfull\n\n","------------------------------------------------------------------------------------")
-            }
-            .addOnFailureListener {
-                Log.d("Failure\n\n","------------------------------------------------------------------------------------------")
-            }
 
     }
 
